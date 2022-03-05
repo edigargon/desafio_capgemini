ba58f83709b684b6d1e55db83c28f30f7d411323
Edigar Gonçalves da Silva <5401@CEUMA.EDU.BR>
2022-03-04 21:16:14 -0300

    Subindo as questões solucionadas

---
+++ Questao2.py
@@ -0,0 +1,12 @@
+'''
+Dado um vetor de inteiros n e um inteiro qualquer x.
+Construa um algoritmo que determine o número de elementos pares
+do vetor que tem uma diferença igual ao valor de x.
+
+Exemplos:
+Entrada: n = [1,5,3,4,2]
+Saida: 3
+
+Explicação:
+Existem 3 pares de inteiros no vetor com uma diferença de 2: [5, 3], [4, 2] e [3, 1].
+'''
